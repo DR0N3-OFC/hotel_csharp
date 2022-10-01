112897e462dda57ddcec7ae378fc8ad065136591
Bruno <dronecraftbr10@gmail.com>
2022-10-01 16:56:21 -0300

    Commit final

--- Program.cs
+++ Program.cs
@@ -6,20 +6,32 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Console.WriteLine("Quantas pessoas serão hospedadas?");
+int quantidadeDePessoas = Convert.ToInt32(Console.ReadLine());
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+for (int i = 0; i < quantidadeDePessoas; i++)
+{
+    Console.WriteLine($"Digite o nome do {i+1}º hóspede.");
+    string nome = Console.ReadLine();
+    hospedes.Add(new Pessoa(nome));
+}
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
-
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
-
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+try
+{
+	// Cria a suíte
+	Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 30);
+	
+	// Cria uma nova reserva, passando a suíte e os hóspedes
+    Console.WriteLine("Determine a quantidade de dias para a reserva: ");
+	Reserva reserva = new Reserva(diasReservados: Convert.ToInt32(Console.ReadLine()));
+	reserva.CadastrarSuite(suite);
+	reserva.CadastrarHospedes(hospedes);
+	
+	// Exibe a quantidade de hóspedes e o valor da diária
+	Console.WriteLine($"Quantidade de hóspedes: {reserva.ObterQuantidadeHospedes()}");
+	Console.WriteLine($"Valor da diária: {reserva.CalcularValorDiaria()}");
+}
+catch (Exception ex) // Captura a exceção e mostra a mensagem do erro
+{
+	Console.WriteLine($"Erro ao finalizar reserva: {ex.Message}");
+}
